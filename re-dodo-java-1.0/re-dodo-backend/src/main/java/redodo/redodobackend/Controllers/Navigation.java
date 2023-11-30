8fe6228a05ddb18cf57d71c34ab5581f53eabb30
dprittj <dprittj@gmail.com>
2023-11-30 15:08:29 -0600

    sitemap and usernest routing enabled and working

--- re-dodo-java-1.0/re-dodo-backend/src/main/java/redodo/redodobackend/Controllers/Navigation.java
+++ re-dodo-java-1.0/re-dodo-backend/src/main/java/redodo/redodobackend/Controllers/Navigation.java
@@ -8,58 +8,69 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 public class Navigation {
 
+//    GETs to each page: home, sitemap, login, usernest
+
     @GetMapping()
-    public String dodo_main(){
+    public String displayHomepageRoot(){
         return "homenest";
     }
 
-//    From dodo-main, <Enter> :
-//    kicks to the login/or/register page
+    @GetMapping("/homenest")
+    public String displayHomepageLink(){
+        return "homenest";
+    }
+
+    @GetMapping("/sitemap")
+    public String displaySitemap() {
+        return "sitemap";
+    }
+
     @GetMapping("/login")
-    public String enter(){
+    public String displayLogin(){
         return "login";
     }
 
-//  user either enters credentials (login) or registers; if entering credentials see LOGIN immediately below; if registering, go down to REGISTER
 
-//  LOGIN
+    // LOGIN: user either enters credentials (login) or registers; if entering credentials see LOGIN immediately below; if registering, go down to REGISTER
+    @GetMapping("/usernest")
+    public static String goToProfile(String username, Integer userId, String userProfile){
+        // NEEDS: retrieve user data via data-controller, and populate dodo-user-nest via Nest controller
+        return "usernest";
+    }
 
+
+    // LOGIN: user enters data for POST-request to Data controller:
     @PostMapping("dodo-landing-pad/profile")
     public static void login(String username, String password){
-//        1.0    send to data-controller for verification; and:
-//        1.1    if verified, send data to goToProfile() nav controller method below.
+    //  1.0    send to data-controller for verification; and:
+    //  1.1    if verified, send data to goToProfile() nav controller method below.
 
-//        1.101  if unverified, try again, and:
-//        1.102  if still unverified, send back enter() method above with error message;
+    //  1.101  if unverified, try again, and:
+    //  1.102  if still unverified, send back enter() method above with error message;
     }
 
-    @GetMapping("dodo-user-nest")
-    public static String goToProfile(String username, int userId, String userProfile){
-//      retrieve user data via data-controller, and populate dodo-user-nest via Nest controller
-        return userProfile;
-    }
 
     @GetMapping("dodo-landing-pad/bird-unknown")
     public static String birdUnknown(){
 
-//        generate error message, and:
-//        send user back to dodo-landing-pad (ie, login/register) to try again
-//        see enter() method above;
+    //  generate error message, and:
+    //  send user back to dodo-landing-pad (ie, login/register) to try again
+    //  see enter() method above;
         return "dodo-landing-pad";
     }
 
-//    REGISTER
 
+    // REGISTER
     @PostMapping("dodo-landing-pad/register")
     public static void newUser(){
-//        kick to createNew data-controller;
-//        take user to newProfile controller to create profile
-
+    //  kick to createNew data-controller;
+    //  take user to newProfile controller to create profile
     }
+
 
     @GetMapping("dodo-user-nest/new")
     public static String newProfile(){
-//        takes user to new profile method in Nest controller
+    //  takes user to new profile method in Nest controller
         return "newUserNest.html";
     }
 
